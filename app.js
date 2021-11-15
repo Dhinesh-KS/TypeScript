0c762f0ff391ae46167d8b5e8e4b9960c325413a
Dhinesh KS <dhinaksd@gmail.com>
2021-11-15 20:10:35 +0530

    Primary core types

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("test hi");
+console.log("--start--");
+function addfn(n1, n2, display, phrase) {
+    if (display)
+        console.log(n1 + n2, phrase);
+}
+var number1;
+number1 = 1;
+addfn(number1, 2, false, "result");
--- app.ts
+++ app.ts
@@ -1,1 +1,9 @@
-console.log("test hi");
+console.log("--start--");
+
+function addfn(n1: number, n2: number, display: boolean, phrase: string) {
+  if (display) console.log(n1 + n2, phrase);
+}
+
+let number1: number;
+number1 = 1;
+addfn(number1, 2, false, "result");
